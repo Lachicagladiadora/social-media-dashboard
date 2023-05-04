340ffb487528008840e8924d54acdf7e42725082
Lachicagradiadora <200379@unsaac.edu.pe>
2023-05-03 19:56:23 -0500

    dark mode is functional with toogle

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const checkboxElement = document.getElementById("checkbox")
 
-// setupCounter(document.querySelector('#counter'))
+const darkMode = () => {
+    window.matchMedia('(prefers-color-scheme: dark)')
+    console.log('hallo! dark-mode is true')
+    checkboxElement.setAttribute('checked',true)
+}
+darkMode()
+
+const darkTogle = () => {
+    document.body.classList.toggle('is-dark-mode')
+}
+
+checkboxElement.addEventListener('change', darkTogle)
+
 
