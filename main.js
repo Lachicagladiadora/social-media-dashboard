6bb0b29b04dc7327d0f61dc82445a5c6ba82fe3e
Lachicagladiadora <200379@unsaac.edu.pe>
2023-10-24 17:45:14 -0500

    fix: style, dark mode and other things

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-import './style.css'
+import "./style.css";
 
-const bodyElement = document.getElementById("body")
-const checkboxElement = document.getElementById("checkbox")
+const checkboxElement = document.getElementById("checkbox");
 
 const darkMode = () => {
-    window.matchMedia('(prefers-color-scheme: dark)')
-    console.log('hallo! dark-mode is true')
-    checkboxElement.setAttribute('checked',true)
-}
-darkMode()
+  window.matchMedia("(prefers-color-scheme: dark)");
+  console.log("hallo! dark-mode is true");
+  checkboxElement.setAttribute("checked", true);
+};
+darkMode();
 
 const darkTogle = () => {
-    bodyElement.classList.toggle('is-dark-mode')
-}
+  document.body.classList.toggle("is-dark-mode");
+};
 
-checkboxElement.addEventListener('change', darkTogle)
+checkboxElement.addEventListener("change", darkTogle);
